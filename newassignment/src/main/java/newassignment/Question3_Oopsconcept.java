3cbc1a78c26055cc342551422b720b08919c48ac
Shubham Khule <shubham.khule@kpipartners.com>
2022-10-03 23:41:08 -0700

    final commit

--- newassignment/src/main/java/newassignment/Question3_Oopsconcept.java
+++ newassignment/src/main/java/newassignment/Question3_Oopsconcept.java
@@ -2,17 +2,6 @@ package newassignment;
 
 	
 	public class Question3_Oopsconcept extends Question3_ParentClass implements Question3_Interface{
-		//in line number 3 we do multiple inharitance as well multi level inharitance 
-		//In this code I include all 4 piller of Oops concept 
-		    //1-Inheritance
-		    //1.1-multiple Inheritance
-		    //1.2-multi level Inheritance
-		    //2.Abstraction (with AbstractQuestion3 class I implement abstraction mathos as well class)
-		    //3.Polymorphism
-		    //3.1 override polymorphism(line 18 show )
-		    //3.2 overloading polymorphism(line 30 show )
-		    //4.Incapsulation as well.
-		    
 		    
 
 		    @Override
--- newassignment/src/main/java/newassignment/Question3_ParentClass.java
+++ newassignment/src/main/java/newassignment/Question3_ParentClass.java
@@ -1,6 +1,6 @@
 package newassignment;
 
-	public class Question3_ParentClass extends  Question3_AbstractClass{
+	public class Question3_ParentClass extends Question3_AbstractClass{
 
 	    @Override
 	    public String num(String n) {
